97c81d8f065d5765da552c8898fcac7e3708e90a
alpha9 <mr.saypadith@gmail.com>
2022-11-21 15:37:20 +0700

    add barcode

--- lib/blue_print_pos.dart
+++ lib/blue_print_pos.dart
@@ -178,7 +178,7 @@ class BluePrintPos {
   }
 
   Future<void> printBarcode(
-    List<dynamic> data, {
+    List<int> data, {
     int size = 120,
     int feedCount = 0,
     bool useCut = false,
@@ -187,7 +187,7 @@ class BluePrintPos {
     final CapabilityProfile profile = await CapabilityProfile.load();
     final Generator generator = Generator(PaperSize.mm58, profile);
     // final List<int> barData = data;
-    bytes += generator.barcode(Barcode.code128(data));
+    bytes += generator.barcode(Barcode.codabar(data));
     // final List<int> byteBuffer = await _getQRImage(data, size.toDouble());
 
     _printProcess(bytes);
